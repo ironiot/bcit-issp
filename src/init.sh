2031a730a2c3ad045b3dc0aa36a6f30199a3ecaa
Amin Jafarpour <a.jafarpour2001@gmail.com>
2026-04-29 12:39:44 -0700

    added docs/diagrams.drawio

---
+++ src/init.sh
@@ -0,0 +1,8 @@
+# For Debian-based systems only:
+sudo apt-get install bluetooth bluez blueman python-serial
+# python-wxgtk2.8 python-wxtools wx2.8-i18n libwxgtk2
+python3 -m venv venv
+source ./venv/bin/activiate
+pip install obd
+
+
