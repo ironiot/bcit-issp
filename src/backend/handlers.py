11d1e01e9ccac629a9bc3eff763325fc87c6a6c5
Felix <pnguyen72@my.bcit.ca>
2026-05-04 16:48:13 -0700

    WIP

--- src/backend/handlers.py
+++ src/backend/handlers.py
@@ -1,9 +1,25 @@
-from db import Vehicle
+from db import DriveCycle, Vehicle
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 
-def get_vehicles(session: Session):
-    """Return all known vehicles."""
+class QueryHandler:
+    def __init__(self, session: Session):
+        self.session = session
 
-    return list(session.scalars(select(Vehicle)))
+    def get_vehicles(self):
+        """Return all known vehicles."""
+
+        return list(self.session.scalars(select(Vehicle)))
+
+    def get_drive_cycles(self, vin: str, limit: int):
+        """Return drive cycles for a given VIN, ordered by start time descending."""
+
+        return list(
+            self.session.scalars(
+                select(DriveCycle)
+                .where(DriveCycle.vin == vin)
+                .order_by(DriveCycle.start_time.desc())
+                .limit(limit)
+            )
+        )
