95e82fabcd30dabf9ed96dee24a3496708014308
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-07 13:44:58 -0700

    add test stubs and package files

---
+++ src/backend/test/test_db_reader.py
@@ -0,0 +1,29 @@
+import unittest
+from unittest.mock import AsyncMock
+
+from db.reader import DBReader
+
+
+class TestDBReader(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.mock_session = AsyncMock()
+        self.reader = DBReader(self.mock_session)
+
+    async def test_get_all_vehicles(self):
+        ...
+
+    async def test_get_vehicle_exist(self):
+        ...
+
+    async def test_get_vehicle_not_exist(self):
+        ...
+
+    async def test_get_vehicle_stats(self):
+        ...
+
+    async def test_get_vehicle_stats(self):
+        ...
+
+if __name__ == "__main__":
+    unittest.main()
+
