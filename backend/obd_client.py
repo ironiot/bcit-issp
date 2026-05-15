58529cb1305e05e665bf46c8bf0e59b5c4f4b780
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-15 13:18:01 -0700

    revert to 35000

--- backend/obd_client.py
+++ backend/obd_client.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass, field
 from datetime import datetime, timezone
 
 import obd
-#socket://127.0.0.1:35000
-OBD_URL = "/dev/pts/6"
+
+OBD_URL = "socket://127.0.0.1:35000"
 
 log = logging.getLogger("obd_client")
 if os.getenv("OBD_DEBUG"):
