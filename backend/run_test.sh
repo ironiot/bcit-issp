8b9d741f417dc45aa296396df1a8c65bbf491780
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-19 09:28:48 -0700

    add more setup

--- backend/run_test.sh
+++ backend/run_test.sh
@@ -2,5 +2,10 @@
 
 set -euxo pipefail
 
+pip install setuptools
+pip install virtualenv
+virtualenv .venv
+source .venv/bin/activate
+
 pip install -r requirements.txt
 python -m test.test_db_reader
