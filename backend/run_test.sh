7fcd952b24ccb032c2efe464184c63cde520c9aa
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-05-19 09:38:45 -0700

    rearrange setup commands

--- backend/run_test.sh
+++ backend/run_test.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-pip install "setuptools<81"
 pip install virtualenv
 virtualenv .venv
 source .venv/bin/activate
+pip install "setuptools<81" wheel
+pip install -r test/requirements.txt
 
-pip install -r requirements.txt
 python -m test.test_db_reader
